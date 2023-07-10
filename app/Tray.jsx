6b35771021ed735a0de92ee58dcb34ef28bbbdc3
Samuel Holmez <samuelholmez@Samuels-MacBook-Air.local>
2023-07-10 14:51:09 -0400

    ADDED reset on trey

--- app/Tray.jsx
+++ app/Tray.jsx
@@ -1,4 +1,4 @@
-import { View, Text,StyleSheet, TouchableOpacity} from "react-native"
+import { View, Text, StyleSheet, TouchableOpacity} from "react-native"
 
 
 export default function Tray({ setSelectRestaurant }){
@@ -8,18 +8,37 @@ export default function Tray({ setSelectRestaurant }){
         setSelectRestaurant(chosen)
     }
 
+    const clear = () => {
+        setSelectRestaurant(0)
+    }
 
     return(
 
         <View style={styles.tray}>
+            <View style={styles.buttonList}>
             <TouchableOpacity onPress={choose} style={styles.button}>
                 <Text style={styles.buttonText}>Shuffle</Text>
             </TouchableOpacity>
+
+            <TouchableOpacity style={styles.resetButton} onPress={clear}>
+                <Text>Reset</Text>
+            </TouchableOpacity>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+resetButton: {
+    backgroundColor: 'grey',
+    paddingHorizontal: 24,
+    paddingVertical: 8,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 13,
+    
+    },
 button: {
     backgroundColor: 'pink',
     paddingHorizontal: 24,
@@ -30,17 +49,23 @@ button: {
     borderColor: 'black'
 },
 buttonText: {
-    
     fontSize: 20,
     color: 'white',
     fontWeight: 400,
 },
 tray: {
+
     width: '100%',
     height: 70,
     backgroundColor: '#e9eeff',
     alignItems: "center",
     justifyContent: 'center'
+},
+buttonList: {
+    display: "flex",
+    flexDirection: "row",
+    width: '80%',
+    justifyContent: 'space-evenly',
 }
 })
 
